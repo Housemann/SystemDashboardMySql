d158ef94490aaa1199b4831bfcccd49f251b0293
Housemann <oliver@vogt.de.com>
2021-06-08 07:46:07 +0200

    Create Table auf collation utf8_general_ci geändert und utf8_decode wieder ausgebaut

--- SystemDashboardMySql/module.php
+++ SystemDashboardMySql/module.php
@@ -473,7 +473,8 @@
             #$content = $content.'<td class="mid">' . utf8_decode($rows['message']).'</td>';
             
             #$content = $content.'<td class="mid">' . $rows['message'].'<br><br>'.$html.'</td>';
-            $content = $content.'<td class="mid_sdb">' . utf8_decode($rows['message']) . $linkbild . '</td>';
+            #$content = $content.'<td class="mid_sdb">' . utf8_decode($rows['message']) . $linkbild . '</td>';
+            $content = $content.'<td class="mid_sdb">' . $rows['message'] . $linkbild . '</td>';
           
             ################################################################################################################################################################################################################################################################################################################################################				
             if($ValueDeleteStatusID == 0) {
@@ -529,7 +530,7 @@
           expirationDate datetime NULL,
           MediaID NVARCHAR(6) NULL,
           AttachmentPath NVARCHAR(400) NULL
-        )";
+        ) DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 
         #Create Indexe anlegen
         $createuniqueindex = "create unique index IF NOT EXISTS ui_id on ips_MessageBoard (id)";
